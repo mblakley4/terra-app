0e8fa73b004e2b232d6a37af18bd530925b1e72c
Mike Blakley <m.blakley@icloud.com>
2019-11-08 12:11:11 -0500

    likes feature implemented

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
-  API_BASE_URL: 'http://localhost:4050/api',
+  API_BASE_URL: 'https://terra-api-50131.herokuapp.com/api',
 }
 
 
-// API_BASE_URL: 'https://terra-api-50131.herokuapp.com/api',
+// 'https://terra-api-50131.herokuapp.com/api',
+// 'http://localhost:4050/api'
