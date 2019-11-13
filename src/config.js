fed35b711ee7d6146985473803a029f0fd4368a9
Mike Blakley <m.blakley@icloud.com>
2019-11-13 13:01:12 -0500

    config dynamically set

--- src/config.js
+++ src/config.js
@@ -2,7 +2,3 @@ export default {
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
     "http://localhost:4050/api"
 }
-
-
-// 'https://terra-api-50131.herokuapp.com/api',
-// 'http://localhost:4050/api'
